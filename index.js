00d8c72a69199001cdeef16ef79ca28de7598bd8
Lucio Paiva <luciopaiva@gmail.com>
2018-10-10 18:18:27 -0300

    Generate uuids for each terminal window
    Tilix needs it to be able to load the json file

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 
+// taken from https://stackoverflow.com/a/2117523/778272
+function uuidv4() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+        const r = Math.random() * 16 | 0;
+        const v = c === "x" ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
 class Terminal {
 
     constructor (command = "") {
         this.directory = "";
         this.height = 0;
         this.overrideCommand = command;
-        this.profile = "";
+        this.profile = uuidv4();
         this.readOnly = false;
         this.synchronizedInput = true;
         this.type = "Terminal";
-        this.uuid = "";
+        // this.uuid = "";
         this.width = 0;
 
         // uncomment if you want to set this field as well
